544fd3ec0b192da9a449bdd7371bac5218bec935
Dotjson01 <sahilali88827@gmail.com>
2026-01-26 12:08:22 +0530

    chapter 6 edited

--- 02_datatypes/chapter_6.py
+++ 02_datatypes/chapter_6.py
@@ -7,3 +7,6 @@ customer_name = "Priya"
 print(f"Order for {customer_name} : {chai_type} Please ! ")
 
 #indexing
+chai_description = "Masala with Kadak Chai"
+print(f"Printing First Word {chai_description[0:6]}")
+print(f"Last Word {chai_description[6:]}")
