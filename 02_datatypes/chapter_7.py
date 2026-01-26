9efcc9b686417fc2ade2eefea5447de2f793845e
Dotjson01 <sahilali88827@gmail.com>
2026-01-26 14:02:25 +0530

    chapter 7 added

---
+++ 02_datatypes/chapter_7.py
@@ -0,0 +1,16 @@
+#tuples ()-> Parenthesis, []-> Bracket, {} -> semi bracket or braces
+# immutable
+
+masala_spices = ("cadamon", "clove", "cinnamon");
+(spice1, spice2, spice3) = masala_spices
+print(f"masala_spices: -> {spice1}, {spice2}, {spice3}")
+
+
+#swapping the values 
+ginger_ratio, cadamon_ratio = 2, 1
+print(f"Ginger Ratio {ginger_ratio} and Cadamon Ratio {cadamon_ratio}")
+ginger_ratio, cadamon_ratio = cadamon_ratio, ginger_ratio
+print(f"Swapping the values: Ginger Ratio {ginger_ratio} Cadamon Ratio {cadamon_ratio}")
+
+#membership checking 
+print(f"Is Ginger present in Spices ? {'Ginger' in masala_spices}");
