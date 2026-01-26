00e9413602e454c2174fef4602ca420e3bb54760
Dotjson01 <sahilali88827@gmail.com>
2026-01-26 12:21:08 +0530

    Edited chapter6

--- 02_datatypes/chapter_6.py
+++ 02_datatypes/chapter_6.py
@@ -10,3 +10,14 @@ print(f"Order for {customer_name} : {chai_type} Please ! ")
 chai_description = "Masala with Kadak Chai"
 print(f"Printing First Word {chai_description[0:6]}")
 print(f"Last Word {chai_description[6:]}")
+
+#if you want reverse a string 
+print(f"Reversing a string {chai_description[::-1]}")
+
+#encoding and decoding 
+label_name = "This is the Secret message from Dark Market"
+encode_name = label_name.encode("utf-16")
+print(f"Label Name : {label_name}");
+print(f"Encode Name : { encode_name}");
+decode_name = encode_name.decode("utf-16");
+print(f"Decoded Message : {decode_name}")
