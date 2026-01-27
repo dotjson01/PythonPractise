402dfc78e1b923c4dc6e1688dab1af68a5047f49
Dotjson01 <sahilali88827@gmail.com>
2026-01-27 23:16:33 +0530

    chapter 7 added

--- 02_datatypes/chapter_7.py
+++ 02_datatypes/chapter_7.py
@@ -1,5 +1,16 @@
 #tuples ()-> Parenthesis, []-> Bracket, {} -> semi bracket or braces
 # immutable
+#key value pair
+
+
+#    A tuple in Python is an immutable ordered collection of elements.
+ #   Tuples are similar to lists, but unlike lists, they cannot be changed after their creation (i.e., they are immutable).
+ ##   Tuples can hold elements of different data types.
+ ##   The main characteristics of tuples are being ordered, heterogeneous and immutable.
+ ##   Creating a Tuple
+  #  A tuple is created by placing all the items inside parentheses (), separated by commas. A tuple can have any number of items and they can be of different data types.
+
+
 
 masala_spices = ("cadamon", "clove", "cinnamon");
 (spice1, spice2, spice3) = masala_spices
