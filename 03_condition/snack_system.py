eac4e279f48351092e83bd8f5b8900422c1e4f48
Dotjson01 <sahilali88827@gmail.com>
2026-01-28 22:57:41 +0530

    snack file added

---
+++ 03_condition/snack_system.py
@@ -0,0 +1,12 @@
+# A local system cafe wants a program that suggests a snack . If a customer asks for a cookies or samosa it confirms the order 
+# otherwise it says it's not available
+
+# take snack input 
+# if it's cookies or samosa confirms the order 
+# else show unavailability 
+
+snacks = ['samosa', 'cookies']
+if 'ginger' in snacks:
+    print(f"Yes Available ")
+
+else :  print(f"Sorry unavailable Can try later ")
