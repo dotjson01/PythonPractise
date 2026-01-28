38d7dacb7d73fca2013b068a5ab5ed1fe80a53e6
Dotjson01 <sahilali88827@gmail.com>
2026-01-28 22:46:09 +0530

    mini project file added

---
+++ 03_condition/mini_project.py
@@ -0,0 +1,9 @@
+# you are creating a notification system for smart kettle
+# reminds when boils completed
+# a variable kettle_boiled = true
+# if boils shows : " Kettle done "
+
+kettle_boiled = False;
+if kettle_boiled : 
+    print(f"Time to make some chai")
+else : print(f" Buy some tea then make chai")
