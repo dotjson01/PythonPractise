e8e08acfcfc64187d0c99c30f70b6dcb1c77658c
Dotjson01 <sahilali88827@gmail.com>
2026-01-30 20:49:46 +0530

    another way

--- 03_condition/deliverfree.py
+++ 03_condition/deliverfree.py
@@ -1,4 +1,11 @@
 # use ternary operator 
+# if order_amount > 300 then delivery is free else have to pay 30/- for delivery
+#
 
+# this is mannual way 
 order_amount = 202
 print(f"Delivery is free") if order_amount > 300 else print(f"Have to pay 30/- for delivery")
+
+# another way 
+delivery_fee = 0 if order_amount > 300 else 30
+print(f"Delivery fee is {delivery_fee}")
