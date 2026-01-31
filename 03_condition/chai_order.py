600259aa3451000cc26a6cc8b01207fbb517f482
Dotjson01 <sahilali88827@gmail.com>
2026-01-31 21:18:16 +0530

    chai order

---
+++ 03_condition/chai_order.py
@@ -0,0 +1,11 @@
+# recieve a order of list 
+# print a name one by one in order format 
+# use a list of names
+#print order by names
+
+order_list = ["Rajesh ", "Priya", "Muthu", "Ramesh", "Suresh"]
+
+#print order by names
+for name in order_list :
+    print(f"Order by names {name}")
+
