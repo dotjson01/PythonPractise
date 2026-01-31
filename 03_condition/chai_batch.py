2052f912435528cd76be43bb9bd8b944eb8463ed
Dotjson01 <sahilali88827@gmail.com>
2026-01-31 21:18:48 +0530

    chai batch

---
+++ 03_condition/chai_batch.py
@@ -0,0 +1,8 @@
+# chai maker makes batch of chai every day in 15min
+# you want stimulate 4 batches
+
+for batch in range(1,5) : 
+    print(f"Batch running {batch}")
+    for i in range(1,11) : 
+        print(f"Serving chai to token number {i}")
+
