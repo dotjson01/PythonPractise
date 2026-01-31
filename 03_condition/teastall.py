32ee92b129a59e6edf7ecd6d078a9aebfb5fa595
Dotjson01 <sahilali88827@gmail.com>
2026-01-31 20:52:43 +0530

    tea committed

---
+++ 03_condition/teastall.py
@@ -0,0 +1,6 @@
+# tea stall owner digital token display
+# for every customer in line , a token number is printed and chai is served 
+# use for loop to generate token number from 0 to 10 using range ()
+# serving chain to token #number
+
+for i in range(1,11): print(f"Serving chai to token number {i}")
