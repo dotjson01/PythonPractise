888221adf64b427209cb8ee86888e52081806670
Dotjson01 <sahilali88827@gmail.com>
2026-02-22 22:49:58 +0530

    function meaning

---
+++ 04_function/function_meaning.py
@@ -0,0 +1,6 @@
+# function are just a wrapper -> reusable the code
+# why function created ?
+# where defining ?
+# break the task into smaller chunks using well-named functions
+# create reusable and modular using def
+# improve readability, tracebility, and maintainability with function
