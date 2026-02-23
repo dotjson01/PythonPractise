ee567f65927d5981c3b908fc6443040a7b187509
Dotjson01 <sahilali88827@gmail.com>
2026-02-23 22:59:51 +0530

    04_cup_billing file

---
+++ 04_function/04_cup_billing.py
@@ -0,0 +1,11 @@
+# you sell different cup sizes 
+# instead of writing formulas everwhere , let create a function
+# calculate_bill (cups, price_per_cup)
+# return total bill
+# use this function for multiple orders
+
+def calculate_bill(cups, price_per_cup) :
+    return print(f"{cups* price_per_cup}")
+
+
+calculate_bill(4, 20)
