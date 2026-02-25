f5db3f25835f2d976332ebe2065b0af4692aae8f
Dotjson01 <sahilali88827@gmail.com>
2026-02-25 20:49:51 +0530

    return file updated

--- 04_function/07_return.py
+++ 04_function/07_return.py
@@ -2,6 +2,17 @@
 
 
 def re():
-    return "I am returning some values and this is what i want"
+    # return "I am returning some values and this is what i want"
+    print("This is print")
+    # noting implicity returns None
+
 
 print(re())
+
+
+def chai_status():
+    return 200, 10
+
+sold, remaining = chai_status()
+print(f"Sold: {sold}")
+print(f"Remaining: {remaining}")
