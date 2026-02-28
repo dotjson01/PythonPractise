db79486251628ebfa6fa2d32187206aace394995
Dotjson01 <sahilali88827@gmail.com>
2026-02-28 19:28:02 +0530

    discount.py

---
+++ 06_chai_business/utils/discounts.py
@@ -0,0 +1,9 @@
+def elachi_chai_discount(price):
+    print("Elachi Chai Discount Applied\
+          and Enjoy your chai")
+    return price * 0.9
+
+def garam_chai_discount(price):
+    print("Garam Chai Discount Applied\
+          and Enjoy your chai")
+    return price * 0.85
