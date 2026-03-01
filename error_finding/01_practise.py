1066c5f950032b528fce492b0509fd4b28ce9ba2
Dotjson01 <sahilali88827@gmail.com>
2026-03-01 20:43:44 +0530

    two files updated

---
+++ error_finding/01_practise.py
@@ -0,0 +1,17 @@
+
+'''
+a=3
+   print(a)
+b= 4
+   print(b)
+s= a+ b
+   print(s)
+
+'''
+
+a=3
+print(a)
+b= 4
+print(b)
+s= a+ b
+print(s)
---
+++ error_finding/02_practise.py
@@ -0,0 +1,3 @@
+name = str ("Sahil")
+age=str(26)
+print("Your name & age are", name+age)
