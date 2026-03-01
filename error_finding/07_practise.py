8262d6d9255d4d40d776530164768d3e342bc951
Dotjson01 <sahilali88827@gmail.com>
2026-03-01 21:20:26 +0530

    error files modified

---
+++ error_finding/07_practise.py
@@ -0,0 +1,4 @@
+a, b = 12, 13
+print (print(a+b))
+
+
---
+++ error_finding/08_practise.py
@@ -0,0 +1,28 @@
+a, b, c = 10, 20, 30
+p, q, r = c -5, a+3, b - 4
+print ('a, b, c :', a, b, c, end='')
+print ('p, q, r:', p, q, r)
+
+a= type(10)
+print(a)
+
+a= type(10.5)
+print(a)
+
+a= type("10.5")
+print(a)
+
+a= type('10.5')
+print(a)
+
+a  = type(int(0))
+print(a)
+
+a= type (float(0))
+print(a)
+
+a= type (complex(0))
+print(a)
+
+a = type(3/2)
+print(a)
