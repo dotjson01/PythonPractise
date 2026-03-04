1bf79a67213c7ba20486b5f6cbdd78e5815ca629
Dotjson01 <sahilali88827@gmail.com>
2026-03-04 22:21:41 +0530

    05_generator_comprehension.py

---
+++ 07_comprehension/05_generator_comprehension.py
@@ -0,0 +1,16 @@
+# entirely for saving the memory
+# ( expression for item in iterable if condition )
+
+daily_sales = [5, 12, 45, 8, 9, 15]
+# calculate sales above 5 
+# total_cups = () # this is generator comprehension
+total_cups = ( sale for sale in daily_sales if sale > 5 )
+print(total_cups)
+
+# this is showing an output 
+# <generator object <genexpr> at 0x72959f3ee0c0>
+# why because of this -> ( )  that consume and reflect back what you want, not entirely show like list  
+
+total_cups = sum( sale for sale in daily_sales if sale > 5 )
+print(total_cups)
+# the sum is giving one by one
