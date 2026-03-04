ae316b6b28499622d43a2fab281c02626b766b30
Dotjson01 <sahilali88827@gmail.com>
2026-03-04 22:52:01 +0530

    generator

---
+++ 08_generator/01_generator.py
@@ -0,0 +1,25 @@
+#  concept behind ->>>>
+
+# you save your memory
+# lazy evaluation
+# you don't the result immediately 
+# in the generator concept you use yield (keyword) --> one value at a time
+
+
+def serve_chai():
+    yield "Cup 1 : Masala Cup"
+    yield "Cup 2 : Ginger Cup"
+    yield "Cup 3 : Cardmom Cup"
+
+stall = serve_chai()
+
+for cup in stall : 
+    print(cup)
+
+def get_chai_list():
+    yield "Cup1"
+    yield "Cup2"
+    yield "Cup3"
+
+stall2 = get_chai_list()
+print(next(stall2))
