e33fec51e9dc3a8b4ebfedb141dcaa60097ab423
Dotjson01 <sahilali88827@gmail.com>
2026-03-09 20:53:41 +0530

    05_practise.py

--- 05_practise.py
+++
@@ -1,8 +0,0 @@
-days = int(input("Enter the number of days : ")) * 3600 * 24
-hours = int (input("Enter the number of hours : ")) * 3600
-minutes = int(input("Enter the number of minutes : ")) * 60
-seconds = int(input("Enter the number of seconds : "))
-
-total = days + hours + minutes + seconds
-
-print(f"Total number of seconds : {total}")
