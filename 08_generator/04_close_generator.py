7cfc6fd29c46b731a1a128bf93def043496e231c
Dotjson01 <sahilali88827@gmail.com>
2026-03-11 17:08:46 +0530

    modified generator

--- 08_generator/04_close_generator.py
+++ 08_generator/04_close_generator.py
@@ -1,4 +1,28 @@
 # sometimes yield and generator  generate the values then need to borrows 
 def local_chai():
-    yield "Masala Chai"
-    yield "Ginger Chai"
+    yield "Masala Chai"                                                                                     # yield -> the basic purpose of yield is to pause and execution of a function , main guy converting into a generator
+    yield "Ginger Chai"                                                                                     #  next() -> manually getting the next value 
+def imported_chai():                                                                                        # .send -> sending data to generator
+    yield "Match"                                                                                           # yield from -> getting value from another generator, sometimes delegate the tasks
+    yield "Oolang"                                                                                          # close -> basically used to cleanup the memory and peacefully stop the generator
+
+def full_menu():
+    yield from local_chai()
+    yield from imported_chai()
+
+for chai in full_menu():
+    print (chai)
+
+
+    # we are using try expect 
+
+def stall():
+    try:
+        order = yield "Wating for a chai"
+    except:
+        print( "Stall closed")
+
+
+chai_stall = stall()
+print(next(chai_stall))
+chai_stall.close() #closing generator that is good idea to close this because due to memeory leakage save
