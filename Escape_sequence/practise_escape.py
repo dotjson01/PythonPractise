fd92159933f3b5401322d66b475e3e8a2e002a90
Dotjson01 <sahilali88827@gmail.com>
2026-03-11 17:24:37 +0530

    practise escape

---
+++ Escape_sequence/practise_escape.py
@@ -0,0 +1,12 @@
+# print() to recreate this example
+# you are allowed to use one print()
+
+# Your Learning Path :
+#  | -> Python
+#  | -> AI
+
+
+print("Your Learning Path : \n"
+      "-> Python \n"
+      "-> AI \n" \
+      "-> Data Science")
