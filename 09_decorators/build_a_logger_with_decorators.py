96048c0e7adb83a9cba24e2175cf68d207542791
Dotjson01 <sahilali88827@gmail.com>
2026-03-11 15:19:03 +0530

    09 decorator

---
+++ 09_decorators/build_a_logger_with_decorators.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+def log_function_call(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f" Calling {func.__name__}")
+        result = func(*args, **kwargs)
+        print(f" Finished {func.__name__}")
+        return result
+    return wrapper
+
+
+@log_function_call
+def brew_chai(type):
+    print(f"Brewing {type} Chai")
+
+brew_chai("Masala Chai")
