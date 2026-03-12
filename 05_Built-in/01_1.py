0640c624bbc51af21229c89d4879e093fce2e49d
Dotjson01 <sahilali88827@gmail.com>
2026-03-13 00:08:11 +0530

    Few changes and updates

--- 05_Built-in/01_1.py
+++ 05_Built-in/01_1.py
@@ -9,3 +9,15 @@ print(chai_flavor())
 print(chai_flavor("Ginger"))
 print(chai_flavor.__doc__)
 print(chai_flavor.__name__)
+
+
+name = "Hello"
+number = 1
+
+print(type(name))
+print(type(number))
+
+print(len(name))
+# print(len(number)) it will generate error
+print(name.upper())
+print(number.bit_length())
