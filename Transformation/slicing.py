679617aec531804d074ed904ecbbab04d8e9c44b
Dotjson01 <sahilali88827@gmail.com>
2026-03-14 23:50:28 +0530

    slicing

---
+++ Transformation/slicing.py
@@ -0,0 +1,13 @@
+# slicing
+#  [start : end ]
+
+name = "Photosynthesis"
+'''
+0   1      2    3   4    5   6   7   8   9  10  11    12  13
+P   h      o    t   o    s   y   n   t   h   e   s    i    s 
+-14 -13  -12  -11  -10  -9  -8  -7  -6  -5  -4  -3   -2   -1
+'''
+print(name[0:3])
+
+# you can also perform from negative
+print(name[-5:-1])
