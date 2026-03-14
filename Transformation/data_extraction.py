e28daaba303661ebfd911d8f381015d21c66d921
Dotjson01 <sahilali88827@gmail.com>
2026-03-14 23:30:43 +0530

    data extraction and split

---
+++ Transformation/data_extraction.py
@@ -0,0 +1,24 @@
+# indexing and slicing
+'i am doing positive indexing '
+name = "Hello"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+'''
+print(name[5])
+
+ule>
+    print(name[5])
+          ~~~~^^^
+IndexError: string index out of range
+'''
+
+'''Let's Try Negative indexing '''
+print(name[-5])
+print(name[-4])
+print(name[-3])
+print(name[-2])
+print(name[-1])
---
+++ Transformation/split.py
@@ -0,0 +1,9 @@
+value = "Adam-24-USA"
+print(value.split("-"))
+# it will return list
+
+value2 = "1234, Max, USA, 1970-03-4"
+print(value2.split(","))
+
+# string repeatation
+print("ha" * 3 )
