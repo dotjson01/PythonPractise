bcc2108dbf48ee3aec501557123e70653c926e8d
Dotjson01 <sahilali88827@gmail.com>
2026-03-15 12:04:58 +0530

    open-ended-slicing

---
+++ Transformation/open-ended-slicing.py
@@ -0,0 +1,23 @@
+name = "Hello"
+print(name[-3:-2])
+
+'''
+name is "Hello".
+Indices for "Hello" are:
+H at 0
+e at 1
+l at 2
+l at 3
+o at 4
+
+
+Negative indices refer to elements from the end:
+o at -1
+l at -2
+l at -3
+
+
+'''
+
+date = "2026-09-20"
+print(date[-10:-6])
