04fefdfb05b85dae33dbff7e00f044d9edf06a2b
Dotjson01 <sahilali88827@gmail.com>
2026-03-16 16:49:02 +0530

    checking,checking gmail, validation

---
+++ Searching/checking_api.py
@@ -0,0 +1,10 @@
+# searching the api keyword inside the url to check the endpoint
+
+url = "https://api.company/auth/v1/data"
+checking = "api" in url
+print(checking)
+
+
+url = "https://pi.company/auth/v1/data"
+checking = "api" in url
+print(checking)
---
+++ Searching/validate_@.py
@@ -0,0 +1,12 @@
+# validating a @ inside the mail
+
+mail = "rohan12@gmail.com"
+is_present = "@" in mail
+print(is_present)
+
+
+
+
+mail = "rohan12gmail.com"
+is_present = "@" in mail
+print(is_present)
