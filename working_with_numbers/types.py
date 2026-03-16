e1c2cea0226e04bb019485be304e28ce08dba511
Dotjson01 <sahilali88827@gmail.com>
2026-03-16 22:51:14 +0530

    types

---
+++ working_with_numbers/types.py
@@ -0,0 +1,17 @@
+# type 
+variable = "24"
+print(type(variable))
+
+integer = 45
+print(type(integer))
+
+
+x="24"
+print(type(x))
+print(x*2)
+# 24 is a string , python repeats the text instead of doing math
+
+x="74"
+print(type(x))
+converted_into_integer= int(x)
+print(type(converted_into_integer))
