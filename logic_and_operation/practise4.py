112ef6595eebafc5127ea361365b51c3e6f46f88
Dotjson01 <sahilali88827@gmail.com>
2026-03-21 22:10:42 +0530

    practise3.py

---
+++ logic_and_operation/practise4.py
@@ -0,0 +1,18 @@
+# check if a username is a string, is not None, and is longer than 5 characters
+
+username = ""
+# 1. using isInstance(username, str) checks if string
+# 2. username is not None 
+# 3. len(username) > 5 (length check )
+checking = isinstance(username, str) and username != None and len(username) > 5
+print(checking)
+
+
+
+
+username = "fdfsdf532#!$@%"
+# 1. using isInstance(username, str) checks if string
+# 2. username is not None 
+# 3. len(username) > 5 (length check )
+checking = isinstance(username, str) and username != None and len(username) > 5
+print(checking)
