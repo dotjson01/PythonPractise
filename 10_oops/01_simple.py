5c1ade8a03011715d970f7ec5ca0b4038c172c4a
Dotjson01 <sahilali88827@gmail.com>
2026-03-24 21:26:02 +0530

    simple.py

---
+++ 10_oops/01_simple.py
@@ -0,0 +1,16 @@
+class Chai:
+    pass
+
+class ChaiTime:
+    pass
+
+print(type(Chai))
+print(type(ChaiTime))
+
+ginger_chai = Chai()
+print(type(ginger_chai))
+print(type(ginger_chai) is Chai) 
+
+# <class '__main__.Chai'> 
+
+# ginger_chai  -> is a class  , __main__. -> is a object , .Chai -> of a type class  
