f9a045da8e27ca390e83848ed5f09247490582f5
Dotjson01 <sahilali88827@gmail.com>
2026-03-28 20:37:55 +0530

    decreasing and pass

--- loops/pass.py
+++ loops/pass.py
@@ -4,3 +4,22 @@ we are going to do here something
 
 '''
 
+names = ['john', 'rohan', 'mohan', ' ', 'kumar']
+for name in names:
+    if name == ' ':
+        pass # handle empty value
+    print(f'{name}')
+
+
+'''
+after a week i come here , and meet my team and ask them hey , here we have to do something i can see empty value in data and love to know how to handle it
+'''
+
+'now after 3 years'
+
+
+names = ['john', 'rohan', 'mohan', ' ', 'kumar']
+for name in names:
+    if name == ' ':
+        name = name.replace(' ', 'unknown value')
+    print(f'{name}')
