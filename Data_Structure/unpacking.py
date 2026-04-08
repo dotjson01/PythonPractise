848f5f47b2cf0e1cbc6ff5f301f60ddff64b6c4f
Dotjson01 <sahilali88827@gmail.com>
2026-04-08 21:22:47 +0530

    unpacking py

---
+++ Data_Structure/unpacking.py
@@ -0,0 +1,15 @@
+list = ['Maria', 29, 'Data Engineering', 'Spain']
+
+name, age, role, country = list
+
+print(list)
+print(name)
+print(age)
+
+
+list2 = ['Maria', 29, 'Data Engineering', 'rich', 'millionarie', 'Spain']
+
+name2, *details, country= list2
+
+
+print(*details) # includes the between all items
