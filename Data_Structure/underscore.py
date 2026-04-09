cafb701a7481f2159c7ab9f84bc001ba554763cb
Dotjson01 <sahilali88827@gmail.com>
2026-04-10 00:13:39 +0530

    underscore

--- Data_Structure/underscore.py
+++ Data_Structure/underscore.py
@@ -5,3 +5,14 @@ age, _, city, bo = lsit
 print(age)
 #print(_)
 print(city)
+
+
+# if you don't want that particular part 
+list2 = [23, 'Data Engineering', True, 1.8, 0X212]
+
+age, *_, hexcode = list2
+print(list2)
+
+print(*_)
+print(age)
+print(hexcode)
