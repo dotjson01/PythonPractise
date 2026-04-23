7d7555415cf8cf78f95ae5d7f337ced3c43f0802
Dotjson01 <sahilali88827@gmail.com>
2026-04-23 18:11:36 +0530

    is operator

--- Data_Structure/IS_operator.py
+++ Data_Structure/IS_operator.py
@@ -5,5 +5,15 @@ matrix = [
     ['c', 'd']
 ]
 
-copy_matrix = matrix
-print('Does it is pointing to same level', matrix is copy_matrix)
+# assignement
+copy1 = matrix
+print('Does it is pointing to same level', matrix is copy1)
+
+# shallow copy
+copy2 = matrix.copy()
+print('Same object', matrix is copy2)
+print('Same list', matrix)
+
+# deep copy
+copy2 = matrix.copy()
+copy3 = matrix.copy()
