a4fc1e0cd05dedde127151dafc714ca60178ec8d
Dotjson01 <sahilali88827@gmail.com>
2026-04-28 21:16:59 +0530

    reversed zip file

--- Data_Structure/enumerate.py
+++ Data_Structure/enumerate.py
@@ -3,3 +3,7 @@ print(enumerate(li)) # it will return object
 print(list(enumerate(li))) # this will return list and object
 
 print(list(enumerate(li, start=1))) # this will start from index 1 
+
+
+for index, value in enumerate(li):
+    print(index, value)
