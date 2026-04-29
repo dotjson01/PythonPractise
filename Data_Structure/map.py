3787960db64d11ccf761649782fdcb5b59574d08
Dotjson01 <sahilali88827@gmail.com>
2026-04-29 21:01:48 +0530

    map

--- Data_Structure/map.py
+++ Data_Structure/map.py
@@ -8,3 +8,8 @@ print(list(map(int, numbers)))
 
 numbers = [1, 2, 3]
 print(list(map(str, numbers)))
+
+
+names = [' Marai', 'Lal ', ' Kumar ']
+print(list(map(str.strip, names))) 
+# this is used to remove unwanted space and clean the data
