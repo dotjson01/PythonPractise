d98b4ffd414154611c8438c95260e4efb764b764
Dotjson01 <sahilali88827@gmail.com>
2026-04-29 21:35:07 +0530

    reversed file

--- Data_Structure/reversed.py
+++ Data_Structure/reversed.py
@@ -13,3 +13,6 @@ for l in reversed(lis):
 print(zip(reversed(lis)))
 print(list(zip(lis,num))) # [('a', 1), ('b', 2), ('c', 3)]
 print(list(zip((lis,num)))) # [(['a', 'b', 'c'],), ([1, 2, 3],)]
+
+for l, n in zip(lis, num):
+    print(l,n)
