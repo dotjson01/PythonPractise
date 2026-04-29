7dbb8e836a2c61baf650a42c37024db57f3fe3ca
Dotjson01 <sahilali88827@gmail.com>
2026-04-29 21:21:50 +0530

    filter.py

--- Data_Structure/filter.py
+++ Data_Structure/filter.py
@@ -1,3 +1,9 @@
 letters = ['a','', None, False, 'c', True, bool]
 print(filter(None, letters))
 print(list(filter(None, letters)))
+
+
+items = ['sql', '123', 'python', '442']
+# isalpha keeps only letters (alphabetic) items
+c = list(filter(str.isalpha, items))
+print(c)
