1724a86f9df74a2c27219aa257a143a7ba756196
Dotjson01 <sahilali88827@gmail.com>
2026-04-30 20:51:05 +0530

    lambda.py

--- Data_Structure/lambda.py
+++ Data_Structure/lambda.py
@@ -1,11 +1,18 @@
-multiple = lambda x: x*2
+def multiple(x): return x*2
+
+
 print(multiple(2))
 
-add = lambda x, y: x+y
-print(add(1,2))
+
+def add(x, y): return x+y
 
 
-check = lambda i: i in 'python'
+print(add(1, 2))
+
+
+def check(i): return i in 'python'
+
+
 print(check('n'))
 print(check('yy'))
 
@@ -18,19 +25,22 @@ print(p.replace('$), '')
 '''
 
 prices = ['$12.50', '$9.99', '$100.00']
-print ( list ( map ( lambda p: float(p.replace('$', '')), prices)))
-
+print(list(map(lambda p: float(p.replace('$', '')), prices)))
 
 
 # lambda + filter
 prices = [120, 30, 3000, 60]
 'remove all prices lower than 100'
-c= list ( filter ( lambda p: p>=100, prices ))
+c = list(filter(lambda p: p >= 100, prices))
 print(c)
 
 
 # keep only students with scores higher than 70
-students =  [['maths', 90],
-             ['Kumar', 90], 
-             ['Max', 60] ]
-print ( list ( filter(lambda p : p[1] > 70 , students ) ) )
+students = [['maths', 90],
+            ['Kumar', 90],
+            ['Max', 60]]
+print(list(filter(lambda p: p[1] > 70, students)))
+
+# print(list(filter(lambda x: start="m", students)))
+print ( students[1][0].startswith('t') ) # false
+print ( students[2][0].startswith('M') ) # true
