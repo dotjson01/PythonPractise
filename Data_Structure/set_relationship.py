26442261339064cfcf396b7488a6529e230665a3
Dotjson01 <sahilali88827@gmail.com>
2026-05-03 16:17:57 +0530

    set relationship

--- Data_Structure/set_relationship.py
+++ Data_Structure/set_relationship.py
@@ -10,5 +10,11 @@ print(a.issubset(b))
 
 print(b.issuperset(a))
 
-
+# if b is the superset of a means b comes in a
 print(b.issuperset(a))
+
+
+# isdijoint() returns TRUE if both sets share no items (No Overalapping)
+a = {45, 44, 5, 30}
+b= {7, 66, 78}
+print(a.isdisjoint(b)) # False
