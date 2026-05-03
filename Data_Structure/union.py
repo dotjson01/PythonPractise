754d51b5cf226e36da41f3b610354372c8378415
Dotjson01 <sahilali88827@gmail.com>
2026-05-03 10:14:13 +0530

    union

--- Data_Structure/union.py
+++ Data_Structure/union.py
@@ -1,4 +1,14 @@
 a= {10, 20, 3, 55, 70}
-b= {30, 40, 66, 78}
+b= {30, 20, 3, 78}
 
 print (a.union(b))
+# use can use | 
+print(a | b)
+
+print (a.intersection(b))
+print ( a.difference(b) )
+print (b.difference(a))
+
+print(a-b)
+print(b-a)
+print(a.symmetric_difference(b))
