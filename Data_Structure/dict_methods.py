7f15d94434b7fe84fd046714a0f212af657dc1c4
Dotjson01 <sahilali88827@gmail.com>
2026-05-03 16:52:41 +0530

    dict methods

---
+++ Data_Structure/dict_methods.py
@@ -0,0 +1,14 @@
+# 
+user = {'id':  1, 'age':45, 'class':'A'}
+print(user)
+ # print(user['name']) # it will throw error if keyword not found yet
+# but if you want that not to throw error, sliently return NONE like that use .get()
+
+print(user.get('name'))
+# you can also use 
+
+print(user['id'])
+
+# in Operator -> Checks or tests if the key is inside the dictionary
+print('age' in user) # True
+print('name' in user) # False
