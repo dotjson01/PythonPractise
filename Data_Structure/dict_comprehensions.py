af296cdf848570fc35e02c2342a4a9cdb46de0d9
Dotjson01 <sahilali88827@gmail.com>
2026-05-05 22:15:22 +0530

    dict - comprehension

---
+++ Data_Structure/dict_comprehensions.py
@@ -0,0 +1,22 @@
+# Challenge : Keep Only String Values & Convert Them to UPPERCASE
+
+user = {
+    'id' : 1,
+    'name' : "John",
+    "age" : 30,
+    "city" : "Berlin"
+}
+
+# 3 Components : key value expression, a loop and and on optional condition
+
+user_str = {
+    # Expression
+    # Loop
+    # Filter
+
+    k:v.upper() # Expression
+    for k, v in user.items() # Loop
+    if isinstance(v,str) # Filter
+}
+
+print(user_str)
