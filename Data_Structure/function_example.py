db973510c10279df641f20ac8baf6f894d2b3df1
Dotjson01 <sahilali88827@gmail.com>
2026-05-06 20:50:50 +0530

    function

---
+++ Data_Structure/function_example.py
@@ -0,0 +1,14 @@
+# now dealing with functions
+
+def make_coffee():
+    print("Start Machine")
+    print("Make Coffee")
+    print("Add Milk")
+    print("Enjoy it")
+
+
+print("Wake Up")
+make_coffee()
+print("Working for a While")
+make_coffee()
+make_coffee()
