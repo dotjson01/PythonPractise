acb312be9760e8ce1292d0d616f5b5ab17ee20d5
Dotjson01 <sahilali88827@gmail.com>
2026-05-06 21:25:51 +0530

    paramets and arguments

---
+++ Data_Structure/paramters_and_arguments.py
@@ -0,0 +1,8 @@
+# parametes -> Functions Definition, PlaceHolder, defines what function expects
+# Arguments -> Function Call, Value (Fills Placeholder), Provides Functions with values
+
+
+def greet(name):
+    print("Hello " + name, 'Welcome to Community How can I help you')
+
+call = greet('Raju')
