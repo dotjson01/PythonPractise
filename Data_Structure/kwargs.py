b68da265b578e75df35b897fba05a98eef0fa3d7
Dotjson01 <sahilali88827@gmail.com>
2026-05-13 20:25:31 +0530

    kwargs

--- Data_Structure/kwargs.py
+++ Data_Structure/kwargs.py
@@ -5,3 +5,14 @@ details(name="Sahil", email = "@gmail", age = 45, indian = True)
 
 # ** When to use ** 
 # Different type of values
+
+
+def create_user(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+
+
+create_user(first_name = "Mo",
+            last_name = "sahil",
+            age=33,
+            country = "Egypt")
