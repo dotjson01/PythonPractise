ac1e2a3f1be7053fcdb5aa0ec5a12ee774554847
Dotjson01 <sahilali88827@gmail.com>
2026-05-14 20:04:59 +0530

    return.py

--- Data_Structure/return.py
+++ Data_Structure/return.py
@@ -6,3 +6,15 @@ def multiple_factor(x):
 z= 6
 print(z)
 
+
+def clean_name(name):
+    cleaned = name.strip().lower()
+    return cleaned
+
+print(clean_name) # it will gonna treat functions as object 
+print(clean_name("Mar")) # it will gonna pass as argument which means following all parameters
+
+
+
+cls=clean_name("Maria") #passing argument and storing the values into cls variable
+print(cls)
